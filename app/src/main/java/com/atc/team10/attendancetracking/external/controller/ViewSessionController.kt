66dab6d44809fc20d1775ad74845f5257646b50b
dbackspace <dbackspace.hust@gmail.com>
2024-01-10 10:08:01 +0700

    Apply empty list if catch exception

--- app/src/main/java/com/atc/team10/attendancetracking/external/controller/ViewSessionController.kt
+++ app/src/main/java/com/atc/team10/attendancetracking/external/controller/ViewSessionController.kt
@@ -2,7 +2,6 @@ package com.atc.team10.attendancetracking.external.controller
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.atc.team10.attendancetracking.data.FakeDataSource
 import com.atc.team10.attendancetracking.data.model.response.SessionResponse
 import com.atc.team10.attendancetracking.data.remote.ApiHelper
 import com.atc.team10.attendancetracking.utils.AppExt.sendLog
@@ -35,11 +34,16 @@ class ViewSessionController : AbsController() {
                     val message = response.message()
                     withContext(Dispatchers.Main) {
                         isLoading.updateValue(false)
-                        listSession.updateValue(FakeDataSource.getListSession())
+                        listSession.updateValue(emptyList())
                     }
+                    sendLog("get student session message1 ${message}")
                 }
             } catch (e: Exception) {
-                sendLog("get student session message ${e.message}")
+                sendLog("get student session message2 ${e.message}")
+                withContext(Dispatchers.Main) {
+                    isLoading.updateValue(false)
+                    listSession.updateValue(emptyList())
+                }
             } finally {
                 withContext(Dispatchers.Main) {
                     isLoading.updateValue(false)
